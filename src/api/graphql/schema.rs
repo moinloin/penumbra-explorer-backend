7fc589d3958a75d607215a08e8f9b90102000c7b
jumpiix <david@unterguggenberger.ch>
2025-03-13 18:07:48 +0100

    feat: Implement GraphQL schema builder

--- src/api/graphql/schema.rs
+++ src/api/graphql/schema.rs
@@ -4,7 +4,6 @@ use sqlx::PgPool;
 use crate::api::graphql::{
     context::ApiContext,
     resolvers::QueryRoot,
-    scalars::register_scalars,
 };
 
 /// Type alias for the complete GraphQL schema
@@ -12,11 +11,7 @@ pub type PenumbraSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 
 /// Create a new GraphQL schema with the given database pool
 pub fn create_schema(db_pool: PgPool) -> PenumbraSchema {
-    let mut builder = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
-        .data(ApiContext::new(db_pool));
-
-    // Register custom scalars
-    builder = register_scalars(builder);
-
-    builder.finish()
+    Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(ApiContext::new(db_pool))
+        .finish()
 }
