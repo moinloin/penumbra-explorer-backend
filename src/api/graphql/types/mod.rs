3ab613def43d4cfaf57b52e50bb085b8e0e315d0
jumpiix <david@unterguggenberger.ch>
2025-03-13 09:27:42 +0100

    feat: organize GraphQL type definitions

---
+++ src/api/graphql/types/mod.rs
@@ -0,0 +1,15 @@
+mod asset;
+mod block;
+mod event;
+mod transaction;
+mod inputs;
+mod unions;
+mod enums;
+
+pub use asset::*;
+pub use block::*;
+pub use event::*;
+pub use transaction::*;
+pub use inputs::*;
+pub use unions::*;
+pub use enums::*;
