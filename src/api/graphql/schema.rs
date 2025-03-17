6ccef95a7cf691ff58ca69f1520b3b41e42a61dd
jumpiix <david@unterguggenberger.ch>
2025-03-17 09:32:41 +0100

    Register all GraphQL object types in schema

--- src/api/graphql/schema.rs
+++ src/api/graphql/schema.rs
@@ -1,9 +1,13 @@
-use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema, SchemaBuilder};
 use sqlx::PgPool;
 
 use crate::api::graphql::{
     context::ApiContext,
     resolvers::QueryRoot,
+    types::{
+        Block, Transaction, TransactionBody, TransactionParameters,
+        Fee, TransactionResult, Event, Action
+    },
 };
 
 /// Type alias for the complete GraphQL schema
@@ -11,7 +15,19 @@ pub type PenumbraSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 
 /// Create a new GraphQL schema with the given database pool
 pub fn create_schema(db_pool: PgPool) -> PenumbraSchema {
-    Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
-        .data(ApiContext::new(db_pool))
-        .finish()
+    let builder = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(ApiContext::new(db_pool));
+
+    // Register all Object implementations
+    let builder = builder
+        .register_output_type::<Block>()
+        .register_output_type::<Transaction>()
+        .register_output_type::<TransactionBody>()
+        .register_output_type::<TransactionParameters>()
+        .register_output_type::<Fee>()
+        .register_output_type::<TransactionResult>()
+        .register_output_type::<Event>()
+        .register_output_type::<Action>();
+
+    builder.finish()
 }
