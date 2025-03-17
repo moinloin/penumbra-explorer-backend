31962f2bad9fbb762e4a6acfc56b5015b10593a5
jumpiix <david@unterguggenberger.ch>
2025-03-17 09:32:41 +0100

    Complete Action union type implementation for frontend

--- src/api/graphql/types/unions.rs
+++ src/api/graphql/types/unions.rs
@@ -10,6 +10,9 @@ pub enum SearchResult {
 #[derive(Union)]
 pub enum Action {
     NotYetSupportedAction(NotYetSupportedAction),
+    IbcRelay(IbcRelay),
+    Output(Output),
+    Spend(Spend),
 }
 
 #[derive(async_graphql::SimpleObject)]
@@ -24,10 +27,35 @@ pub struct IbcRelay {
 
 #[derive(async_graphql::SimpleObject)]
 pub struct Output {
+    pub body: OutputBody,
     pub proof: String,
 }
 
 #[derive(async_graphql::SimpleObject)]
+pub struct OutputBody {
+    pub balance_commitment: String,
+    pub note_payload: NotePayload,
+    pub ovk_wrapped_key: String,
+    pub wrapped_memo_key: String,
+}
+
+#[derive(async_graphql::SimpleObject)]
+pub struct NotePayload {
+    pub encrypted_note: String,
+    pub ephemeral_key: String,
+    pub note_commitment: String,
+}
+
+#[derive(async_graphql::SimpleObject)]
 pub struct Spend {
+    pub auth_sig: String,
+    pub body: SpendBody,
     pub proof: String,
+}
+
+#[derive(async_graphql::SimpleObject)]
+pub struct SpendBody {
+    pub balance_commitment: String,
+    pub nullifier: String,
+    pub rk: String,
 }
