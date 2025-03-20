53e2d720186f5b167673fb7f45e3a240e9a99a73
jumpiix <david@unterguggenberger.ch>
2025-03-20 16:44:33 +0100

    Fix formatting in GraphQL module

--- src/api/graphql/mod.rs
+++ src/api/graphql/mod.rs
@@ -1,9 +1,9 @@
 pub mod context;
-pub mod schema;
-pub mod scalars;
-pub mod types;
 pub mod resolvers;
+pub mod scalars;
+pub mod schema;
+pub mod types;
 
+pub use resolvers::QueryRoot;
 pub use schema::{create_schema, PenumbraSchema};
 pub use types::*;
-pub use resolvers::QueryRoot;
