e70727f7f7d8b6d2d9e39394b76e7d34751e441d
jumpiix <david@unterguggenberger.ch>
2025-03-20 16:44:34 +0100

    Fix formatting in scalars module

--- src/api/graphql/scalars/mod.rs
+++ src/api/graphql/scalars/mod.rs
@@ -1,11 +1,11 @@
 mod datetime;
 
-use async_graphql::{SchemaBuilder, EmptyMutation, EmptySubscription};
 use crate::api::graphql::resolvers::QueryRoot;
+use async_graphql::{EmptyMutation, EmptySubscription, SchemaBuilder};
 
 /// Register all custom scalars with the schema
 pub fn register_scalars(
-    builder: SchemaBuilder<QueryRoot, EmptyMutation, EmptySubscription>
+    builder: SchemaBuilder<QueryRoot, EmptyMutation, EmptySubscription>,
 ) -> SchemaBuilder<QueryRoot, EmptyMutation, EmptySubscription> {
     let builder = datetime::register(builder);
     builder
