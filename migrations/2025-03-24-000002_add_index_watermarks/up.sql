411f0eff774f33b06b6c642bdbbca909945ed1bc
jumpiix <david@unterguggenberger.ch>
2025-03-24 17:56:31 +0100

    Add index_watermarks table for cometindex tracking

---
+++ migrations/2025-03-24-000002_add_index_watermarks/up.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS index_watermarks (
+                                                index_name TEXT PRIMARY KEY,
+                                                version BIGINT NOT NULL
+);
