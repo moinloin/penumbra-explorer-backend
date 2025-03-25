3a6abf7469e4e66a957df2f8d43623bf8ff0face
jumpiix <david@unterguggenberger.ch>
2025-03-25 08:59:33 +0100

    Add down migration for application schema cleanup

---
+++ migrations/2025_03_25_app_schema/down.sql
@@ -0,0 +1,5 @@
+DROP VIEW IF EXISTS explorer_recent_blocks CASCADE;
+DROP VIEW IF EXISTS explorer_transaction_summary CASCADE;
+
+DROP TABLE IF EXISTS explorer_transactions CASCADE;
+DROP TABLE IF EXISTS explorer_block_details CASCADE;
