bfcc66be70aad2d604c65726d78eb6d8396f7bde
jumpiix <david@unterguggenberger.ch>
2025-03-25 09:32:55 +0100

    Add indexes and views migration

--- migrations/2025_03_25_app_schema/down.sql
+++
@@ -1,5 +0,0 @@
-DROP VIEW IF EXISTS explorer_recent_blocks CASCADE;
-DROP VIEW IF EXISTS explorer_transaction_summary CASCADE;
-
-DROP TABLE IF EXISTS explorer_transactions CASCADE;
-DROP TABLE IF EXISTS explorer_block_details CASCADE;
