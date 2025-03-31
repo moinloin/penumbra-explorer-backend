854bd1393316a5a28faca4cebf527fa77bf35385
jumpiix <david@unterguggenberger.ch>
2025-03-31 17:35:14 +0200

    chore: adjust formatting for Rust best practices

--- src/api/graphql/types/transaction.rs
+++ src/api/graphql/types/transaction.rs
@@ -174,9 +174,9 @@ impl DbRawTransaction {
                 tx_hash = $1
             "#,
         )
-            .bind(&tx_hash_bytes)
-            .fetch_optional(db)
-            .await?;
+        .bind(&tx_hash_bytes)
+        .fetch_optional(db)
+        .await?;
 
         if let Some(row) = row_result {
             let tx_hash: Vec<u8> = row.get("tx_hash");
@@ -223,10 +223,10 @@ impl DbRawTransaction {
             LIMIT $1 OFFSET $2
             "#,
         )
-            .bind(limit)
-            .bind(offset)
-            .fetch_all(db)
-            .await?;
+        .bind(limit)
+        .bind(offset)
+        .fetch_all(db)
+        .await?;
 
         let mut transactions = Vec::with_capacity(rows.len());
 
