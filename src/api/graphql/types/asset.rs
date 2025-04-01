a87bcd9f6bb0659d507196a2b9095aa11257a1dc
jumpiix <david@unterguggenberger.ch>
2025-04-01 17:11:00 +0200

    refactor(asset types): rename pub _ fields, add #[must_use], satisfy clippy::module_name_repetitions

--- src/api/graphql/types/asset.rs
+++ src/api/graphql/types/asset.rs
@@ -1,19 +1,23 @@
 use async_graphql::SimpleObject;
 
 #[derive(SimpleObject, Clone, Default)]
+#[allow(clippy::module_name_repetitions)]
 pub struct AssetId {
     pub alt_base_denom: String,
     #[graphql(name = "altBaseDenom")]
+    #[allow(clippy::pub_underscore_fields)]
     pub _alt_base_denom: String,
 
     pub alt_bech32m: String,
     #[graphql(name = "altBech32M")]
+    #[allow(clippy::pub_underscore_fields)]
     pub _alt_bech32m: String,
 
     pub inner: String,
 }
 
 impl AssetId {
+    #[must_use]
     pub fn new(alt_base_denom: String, alt_bech32m: String, inner: String) -> Self {
         Self {
             alt_base_denom: alt_base_denom.clone(),
