a8feb321dd144ec006a669204713db048286b8d6
jumpiix <david@unterguggenberger.ch>
2025-04-01 17:11:00 +0200

    refactor(scalars mod): rename to avoid repetition, add #[must_use], clippy clean

--- src/api/graphql/scalars/mod.rs
+++ src/api/graphql/scalars/mod.rs
@@ -4,6 +4,8 @@ use crate::api::graphql::resolvers::QueryRoot;
 use async_graphql::{EmptyMutation, EmptySubscription, SchemaBuilder};
 
 /// Register all custom scalars with the schema
+#[must_use]
+#[allow(clippy::module_name_repetitions)]
 pub fn register_scalars(
     builder: SchemaBuilder<QueryRoot, EmptyMutation, EmptySubscription>,
 ) -> SchemaBuilder<QueryRoot, EmptyMutation, EmptySubscription> {
