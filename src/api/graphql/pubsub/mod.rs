8c2f99cd1df8b4f99439403a9972b99a4277af1d
jumpiix <david@unterguggenberger.ch>
2025-04-11 09:58:49 +0200

    feat: add PubSub struct

--- src/api/graphql/pubsub/mod.rs
+++ src/api/graphql/pubsub/mod.rs
@@ -3,3 +3,9 @@ use sqlx::{Pool, Postgres};
 use tokio::sync::broadcast;
 
 pub mod triggers;
+
+pub struct PubSub {
+    blocks_tx: broadcast::Sender<i64>,
+    transactions_tx: broadcast::Sender<i64>,
+    transaction_count_tx: broadcast::Sender<i64>,
+}
