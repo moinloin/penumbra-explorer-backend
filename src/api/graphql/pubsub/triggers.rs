c71e5ab83b45cc5f07ae077319ebc6ba8400419e
jumpiix <david@unterguggenberger.ch>
2025-04-11 10:00:31 +0200

    feat: add block listener function

--- src/api/graphql/pubsub/triggers.rs
+++ src/api/graphql/pubsub/triggers.rs
@@ -1,4 +1,5 @@
 use sqlx::{Pool, Postgres};
+use tokio::time::{interval, Duration};
 use tracing::{debug, error, info};
 
 use super::PubSub;
@@ -11,4 +12,25 @@ pub async fn start_triggers(pubsub: PubSub, pool: Pool<Postgres>) {
         listen_for_transactions(pubsub.clone(), pool.clone()),
         listen_for_transaction_count(pubsub, pool)
     );
+}
+
+async fn listen_for_blocks(pubsub: PubSub, pool: Pool<Postgres>) {
+    let mut interval = interval(Duration::from_secs(1));
+    let mut last_height: Option<i64> = None;
+
+    loop {
+        interval.tick().await;
+        
+        match get_latest_block_height(&pool).await {
+            Ok(Some(height)) => {
+                if last_height.is_none() || last_height.unwrap() < height {
+                    debug!("New block detected: {}", height);
+                    pubsub.publish_block(height);
+                    last_height = Some(height);
+                }
+            }
+            Ok(None) => {},
+            Err(e) => error!("Error fetching latest block: {}", e),
+        }
+    }
 }
