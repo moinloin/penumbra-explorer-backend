5270a55e832d762227959f9cbe0c7de0fa8ce54d
jumpiix <david@unterguggenberger.ch>
2025-04-11 10:04:54 +0200

    feat: update schema to use SubscriptionRoot

--- src/api/graphql/schema.rs
+++ src/api/graphql/schema.rs
@@ -1,22 +1,34 @@
-use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use async_graphql::{EmptyMutation, Schema};
 use sqlx::PgPool;
 
 use crate::api::graphql::{
     context::ApiContext,
-    resolvers::QueryRoot,
-    types::{Action, Block, Event, Fee, Transaction, TransactionBody, TransactionParameters},
+    pubsub::PubSub,
+    resolvers::{QueryRoot, SubscriptionRoot},
+    types::{
+        Action, Block, BlockUpdate, Event, Fee, Transaction, TransactionBody, TransactionCountUpdate,
+        TransactionParameters, TransactionUpdate,
+    },
 };
 
 /// Type alias for the complete GraphQL schema
 #[allow(clippy::module_name_repetitions)]
-pub type PenumbraSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+pub type PenumbraSchema = Schema<QueryRoot, EmptyMutation, SubscriptionRoot>;
 
 /// Create a new GraphQL schema with the given database pool
 #[must_use]
 #[allow(clippy::module_name_repetitions)]
 pub fn create_schema(db_pool: PgPool) -> PenumbraSchema {
-    let builder =
-        Schema::build(QueryRoot, EmptyMutation, EmptySubscription).data(ApiContext::new(db_pool));
+    let pubsub = PubSub::new();
+    let pool_clone = db_pool.clone();
+    
+    tokio::spawn(async move {
+        pubsub.clone().start_triggers(pool_clone).await;
+    });
+    
+    let builder = Schema::build(QueryRoot, EmptyMutation, SubscriptionRoot)
+        .data(ApiContext::new(db_pool))
+        .data(pubsub);
 
     let builder = builder
         .register_output_type::<Block>()
@@ -25,7 +37,10 @@ pub fn create_schema(db_pool: PgPool) -> PenumbraSchema {
         .register_output_type::<TransactionParameters>()
         .register_output_type::<Fee>()
         .register_output_type::<Event>()
-        .register_output_type::<Action>();
+        .register_output_type::<Action>()
+        .register_output_type::<BlockUpdate>()
+        .register_output_type::<TransactionUpdate>()
+        .register_output_type::<TransactionCountUpdate>();
 
     builder.finish()
 }
