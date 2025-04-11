2d0fcd6b9607cb1433a6a55b95c42063ad4b8627
jumpiix <david@unterguggenberger.ch>
2025-04-11 10:00:17 +0200

    feat: add start_triggers function

--- src/api/graphql/pubsub/triggers.rs
+++ src/api/graphql/pubsub/triggers.rs
@@ -2,3 +2,13 @@ use sqlx::{Pool, Postgres};
 use tracing::{debug, error, info};
 
 use super::PubSub;
+
+pub async fn start_triggers(pubsub: PubSub, pool: Pool<Postgres>) {
+    info!("Starting subscription triggers");
+    
+    tokio::join!(
+        listen_for_blocks(pubsub.clone(), pool.clone()),
+        listen_for_transactions(pubsub.clone(), pool.clone()),
+        listen_for_transaction_count(pubsub, pool)
+    );
+}
